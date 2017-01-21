3d0b20b1d462c01f757f00c403efe17757405718
pookie <jleo2525@live.com>
2017-01-21 10:13:00 -0500

    adding test for quicksort and perf test with comparable results to issort

--- src/sort/tests/issort.tests.c
+++ src/sort/tests/issort.tests.c
@@ -6,7 +6,7 @@
 
 #define KYEL  "\x1B[33m"
 
-int compare_ints(const void *key1, const void *key2)
+int issort_compare_ints(const void *key1, const void *key2)
 {
 	int k1 = *(int*)(key1);
 	int k2 = *(int*)(key2);
@@ -17,7 +17,7 @@ int compare_ints(const void *key1, const void *key2)
 	return 0;
 }
 
-int compare_ints_desc(const void *key1, const void *key2)
+int issort_compare_ints_desc(const void *key1, const void *key2)
 {
 	int k1 = *(int*)(key1);
 	int k2 = *(int*)(key2);
@@ -28,54 +28,54 @@ int compare_ints_desc(const void *key1, const void *key2)
 	return 0;
 }
 
-static int unsorted_arr[] = {5, 2, 8, 40, 22, 10, 4};
-static int random_unsorted_arr[100000000];
+static int issort_unsorted_arr[] = {5, 2, 8, 40, 22, 10, 4};
+static int issort_random_issort_unsorted_arr[20000];
 
-static clock_t start_times[2];
-static clock_t end_times[2];
+static clock_t issort_start_times[2];
+static clock_t issort_end_times[2];
 
-void fill_random_unsorted_arr(void)
+void fill_issort_random_issort_unsorted_arr(void)
 {
 	srand(time(NULL));
 
-	for(int i = 0; i < 100000000; i++) random_unsorted_arr[i] = rand();
+	for(int i = 0; i < 20000; i++) issort_random_issort_unsorted_arr[i] = rand();
 }
 
 Test(insertion_sort, ascending_sort)
 {
-	issort(&unsorted_arr, 7, sizeof(int), &compare_ints);
+	issort(&issort_unsorted_arr, 7, sizeof(int), &issort_compare_ints);
 
-	cr_assert(unsorted_arr[0] == 2, "element [0] should have value 2 but was %d", unsorted_arr[0]);
-	cr_assert(unsorted_arr[1] == 4, "element [1] should have value 4 but was %d", unsorted_arr[1]);
-	cr_assert(unsorted_arr[2] == 5, "element [2] should have value 5 but was %d", unsorted_arr[2]);
-	cr_assert(unsorted_arr[3] == 8, "element [3] should have value 8 but was %d", unsorted_arr[3]);
-	cr_assert(unsorted_arr[4] == 10, "element [4] should have value 10 but was %d", unsorted_arr[4]);
-	cr_assert(unsorted_arr[5] == 22, "element [5] should have value 22 but was %d", unsorted_arr[5]);
-	cr_assert(unsorted_arr[6] == 40, "element [6] should have value 40 but was %d", unsorted_arr[6]);
+	cr_assert(issort_unsorted_arr[0] == 2, "element [0] should have value 2 but was %d", issort_unsorted_arr[0]);
+	cr_assert(issort_unsorted_arr[1] == 4, "element [1] should have value 4 but was %d", issort_unsorted_arr[1]);
+	cr_assert(issort_unsorted_arr[2] == 5, "element [2] should have value 5 but was %d", issort_unsorted_arr[2]);
+	cr_assert(issort_unsorted_arr[3] == 8, "element [3] should have value 8 but was %d", issort_unsorted_arr[3]);
+	cr_assert(issort_unsorted_arr[4] == 10, "element [4] should have value 10 but was %d", issort_unsorted_arr[4]);
+	cr_assert(issort_unsorted_arr[5] == 22, "element [5] should have value 22 but was %d", issort_unsorted_arr[5]);
+	cr_assert(issort_unsorted_arr[6] == 40, "element [6] should have value 40 but was %d", issort_unsorted_arr[6]);
 }
 
 Test(insertion_sort, descending_sort)
 {
-	issort(&unsorted_arr, 7, sizeof(int), &compare_ints_desc);
+	issort(&issort_unsorted_arr, 7, sizeof(int), &issort_compare_ints_desc);
 
-	cr_assert(unsorted_arr[6] == 2, "element [6] should have value 2 but was %d", unsorted_arr[6]);
-	cr_assert(unsorted_arr[5] == 4, "element [5] should have value 4 but was %d", unsorted_arr[5]);
-	cr_assert(unsorted_arr[4] == 5, "element [4] should have value 5 but was %d", unsorted_arr[4]);
-	cr_assert(unsorted_arr[3] == 8, "element [3] should have value 8 but was %d", unsorted_arr[3]);
-	cr_assert(unsorted_arr[2] == 10, "element [2] should have value 10 but was %d", unsorted_arr[2]);
-	cr_assert(unsorted_arr[1] == 22, "element [1] should have value 22 but was %d", unsorted_arr[1]);
-	cr_assert(unsorted_arr[0] == 40, "element [0] should have value 40 but was %d", unsorted_arr[0]);
+	cr_assert(issort_unsorted_arr[6] == 2, "element [6] should have value 2 but was %d", issort_unsorted_arr[6]);
+	cr_assert(issort_unsorted_arr[5] == 4, "element [5] should have value 4 but was %d", issort_unsorted_arr[5]);
+	cr_assert(issort_unsorted_arr[4] == 5, "element [4] should have value 5 but was %d", issort_unsorted_arr[4]);
+	cr_assert(issort_unsorted_arr[3] == 8, "element [3] should have value 8 but was %d", issort_unsorted_arr[3]);
+	cr_assert(issort_unsorted_arr[2] == 10, "element [2] should have value 10 but was %d", issort_unsorted_arr[2]);
+	cr_assert(issort_unsorted_arr[1] == 22, "element [1] should have value 22 but was %d", issort_unsorted_arr[1]);
+	cr_assert(issort_unsorted_arr[0] == 40, "element [0] should have value 40 but was %d", issort_unsorted_arr[0]);
 }
 
-Test(insertion_sort, big_ascending_sort)
+Test(insertion_sort, big_ascending_sort, .init = fill_issort_random_issort_unsorted_arr )
 {
-	start_times[0] = clock();
+	issort_start_times[0] = clock();
 
-	issort(&random_unsorted_arr, 100000000, sizeof(int), &compare_ints);
+	issort(&issort_random_issort_unsorted_arr, 20000, sizeof(int), &issort_compare_ints);
 
-	end_times[0] = clock();
+	issort_end_times[0] = clock();
 	
-	double elapsed_time = (end_times[0] - start_times[0])/(double)CLOCKS_PER_SEC;
+	double elapsed_time = (issort_end_times[0] - issort_start_times[0])/(double)CLOCKS_PER_SEC;
 
-	printf(KYEL "Sorting 100000000 ints took approximately %f seconds.\n", elapsed_time);
+	printf(KYEL "Insertion sort: Sorting 20000 ints took approximately %f seconds.\n", elapsed_time);
 }
---
+++ src/sort/tests/qksort.tests.c
@@ -0,0 +1,81 @@
+#include <criterion/criterion.h>
+#include <time.h>
+#include <stdio.h>
+
+#include "../qksort.h"
+
+#define KYEL  "\x1B[33m"
+
+int qksort_compare_ints(const void *key1, const void *key2)
+{
+	int k1 = *(int*)(key1);
+	int k2 = *(int*)(key2);
+	
+	if(k1 > k2) return 1;
+	if(k1 < k2) return -1;
+
+	return 0;
+}
+
+int qksort_compare_ints_desc(const void *key1, const void *key2)
+{
+	int k1 = *(int*)(key1);
+	int k2 = *(int*)(key2);
+	
+	if(k1 > k2) return -1;
+	if(k1 < k2) return 1;
+
+	return 0;
+}
+
+static int unsorted_arr[] = {5, 2, 8, 40, 22, 10, 4};
+static int qksort_random_unsorted_arr[1000000];
+
+static clock_t qksort_start_times[2];
+static clock_t qksort_end_times[2];
+
+void fill_qksort_random_unsorted_arr(void)
+{
+	srand(time(NULL));
+
+	for(int i = 0; i < 1000000; i++) qksort_random_unsorted_arr[i] = rand();
+}
+
+Test(quick_sort, ascending_sort)
+{
+	qksort(&unsorted_arr, 7, sizeof(int), 0, 6, &qksort_compare_ints);
+
+	cr_assert(unsorted_arr[0] == 2, "element [0] should have value 2 but was %d", unsorted_arr[0]);
+	cr_assert(unsorted_arr[1] == 4, "element [1] should have value 4 but was %d", unsorted_arr[1]);
+	cr_assert(unsorted_arr[2] == 5, "element [2] should have value 5 but was %d", unsorted_arr[2]);
+	cr_assert(unsorted_arr[3] == 8, "element [3] should have value 8 but was %d", unsorted_arr[3]);
+	cr_assert(unsorted_arr[4] == 10, "element [4] should have value 10 but was %d", unsorted_arr[4]);
+	cr_assert(unsorted_arr[5] == 22, "element [5] should have value 22 but was %d", unsorted_arr[5]);
+	cr_assert(unsorted_arr[6] == 40, "element [6] should have value 40 but was %d", unsorted_arr[6]);
+}
+
+Test(quick_sort, descending_sort)
+{
+	qksort(&unsorted_arr, 7, sizeof(int), 0, 6, &qksort_compare_ints_desc);
+
+	cr_assert(unsorted_arr[6] == 2, "element [6] should have value 2 but was %d", unsorted_arr[6]);
+	cr_assert(unsorted_arr[5] == 4, "element [5] should have value 4 but was %d", unsorted_arr[5]);
+	cr_assert(unsorted_arr[4] == 5, "element [4] should have value 5 but was %d", unsorted_arr[4]);
+	cr_assert(unsorted_arr[3] == 8, "element [3] should have value 8 but was %d", unsorted_arr[3]);
+	cr_assert(unsorted_arr[2] == 10, "element [2] should have value 10 but was %d", unsorted_arr[2]);
+	cr_assert(unsorted_arr[1] == 22, "element [1] should have value 22 but was %d", unsorted_arr[1]);
+	cr_assert(unsorted_arr[0] == 40, "element [0] should have value 40 but was %d", unsorted_arr[0]);
+}
+
+Test(quick_sort, big_ascending_sort, .init = fill_qksort_random_unsorted_arr )
+{
+	qksort_start_times[0] = clock();
+
+	qksort(&qksort_random_unsorted_arr, 1000000, sizeof(int), 0, (1000000 - 1), &qksort_compare_ints);
+
+	qksort_end_times[0] = clock();
+	
+	double elapsed_time = (qksort_end_times[0] - qksort_start_times[0])/(double)CLOCKS_PER_SEC;
+
+	printf(KYEL "Quicksort: Sorting 1000000 ints took approximately %f seconds.\n", elapsed_time);
+}
