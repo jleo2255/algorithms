460beec607769e026cf7027d3672f32f896bd153
pookie <pookie@pookies-MacBook-Pro.local>
2017-02-16 21:22:00 -0500

    adding rest of changes to ctsort and modifying makefile to use gcc

--- src/sort/ctsort.c
+++ src/sort/ctsort.c
@@ -9,15 +9,32 @@ int ctsort(int *data, int size, int max_plus_one)
 	int		i, j;
 
 	// allocate data for counts and temp arrays
-	if ((counts = (int*)malloc(k * sizeof(int))) == NULL) return -1;
+	if ((counts = (int*)malloc(max_plus_one * sizeof(int))) == NULL) return -1;
 	if ((temp = (int*)malloc(size * sizeof(int))) == NULL)
 	{
 		free(counts);
 		return -1;
 	}
 
-	// initialize the counts array
+	// initialize the counts array 
 	for (i = 0; i < max_plus_one; i++) counts[i] = 0;
 
-	
+	// fill the index array with counts
+	for (j = 0; j < size; j++) counts[data[j]] = counts[data[j]] + 1;
+
+	// adjust the counts to be an array of offsets
+	for (i = 1; i < max_plus_one; i++) counts[i] = counts[i - 1] + counts[i];
+
+	for (j = size - 1; j <= 0; j--)
+	{
+		temp[counts[data[j] - 1]] = data[j];
+		counts[data[j]] = counts[data[j]] - 1;
+	}
+
+	memcpy(data, temp, size * sizeof(int));
+
+	free(counts);
+	free(temp);
+		
+	return 0;
 }
