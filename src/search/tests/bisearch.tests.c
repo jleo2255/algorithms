6cd681b05a30f8562681a1a4796c540415a6693e
James <jleo2525@live.com>
2017-04-15 19:46:51 +0000

    adding failure test

--- src/search/tests/bisearch.tests.c
+++ src/search/tests/bisearch.tests.c
@@ -18,7 +18,7 @@ static int compare_strs(const void *str1, const void *str2)
 	else return 0;
 }
 
-Test(bisearch, finds_text)
+Test(bisearch, finds_text_when_present)
 {
 	char *test_string = malloc(36);
 	memcpy((char *)test_string + (0 * 6), "five ", 5);
@@ -30,6 +30,22 @@ Test(bisearch, finds_text)
 
 	int has_str = bisearch(test_string, "six  ", 6, 6, compare_strs);	
 	cr_assert(has_str == 3, "bisearch should find six in sorted str array");
+
+	free(test_string);
+}
+
+Test(bisearch, does_not_find_text_when_present)
+{
+	char *test_string = malloc(36);
+	memcpy((char *)test_string + (0 * 6), "five ", 5);
+	memcpy((char *)test_string + (1 * 6), "four ", 5);
+	memcpy((char *)test_string + (2 * 6), "one  ", 5);
+	memcpy((char *)test_string + (3 * 6), "six  ", 5);
+	memcpy((char *)test_string + (4 * 6), "three", 5);
+	memcpy((char *)test_string + (5 * 6), "two  ", 5);
+
+	int has_str = bisearch(test_string, "sixty", 6, 6, compare_strs);	
+	cr_assert(has_str == -1, "bisearch should not find sixty in sorted str array");
 
 	free(test_string);
 }
